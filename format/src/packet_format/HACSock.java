b471c237251feed219d9f9a90c8476b922b12a7c
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2021-03-20 13:12:04 -0500

    Added documentation

--- format/src/packet_format/HACSock.java
+++ format/src/packet_format/HACSock.java
@@ -8,10 +8,20 @@ public class HACSock {
   DatagramSocket socket = null;
   private static final int PACKET_SIZE = 1028;
   
+  /**
+   * Constructor initializes a socket
+   * @param port port to connect on
+   * @throws SocketException
+   */
   public HACSock(int port) throws SocketException {
     this.socket = new DatagramSocket(port);
   }
   
+  /**
+   * receives incoming packets
+   * @return a HACPack containing the new HACPack
+   * @throws IOException
+   */
   public HACPack receive() throws IOException {
     byte[] incomingData = new byte[PACKET_SIZE];
     DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
@@ -19,6 +29,12 @@ public class HACSock {
     return new HACPack(incomingPacket);
   }
   
+  /**
+   * sends a packet
+   * @param packet the HACPack to send
+   * @return if false, then the HACPack was incomplete
+   * @throws IOException
+   */
   public boolean send(HACPack packet) throws IOException {
     if(packet.getaddress() == null || packet.getPort() == 0)
       return false;
@@ -31,6 +47,9 @@ public class HACSock {
     return true;
   }
   
+  /**
+   * closes the socket
+   */
   public void close() {
     this.socket.close();
   }
